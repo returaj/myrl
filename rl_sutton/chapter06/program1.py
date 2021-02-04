52c66dd0931758fae667f8e39f7308b10595ab46
returaj <returaj123burnwal@gmail.com>
2021-02-04 12:57:52 +0530

    add exercise 6.10 code

--- rl_sutton/chapter06/program1.py
+++ rl_sutton/chapter06/program1.py
@@ -9,19 +9,32 @@ import matplotlib.pyplot as plt
 # length and width of the grid
 LENGTH, WIDTH = 10, 7
 
-# type of action North, South, East, West movement
-MAP_ACTION = {0: (-1, 0), 1: (0, 1), 2: (1, 0), 3: (0, -1)}
+# MAX_DISTANCE
+MAX_DISTANCE = LENGTH*WIDTH
 
-MAP_ACTION_TO_DIR = {0: 'N', 1: 'E', 2: 'S', 3: 'W'}
+EXAMPLE_6_5 = {0: (-1, 0), 1: (0, 1), 2: (1, 0), 3: (0, -1)}
+
+EXERCISE_6_9_1 = {0: (-1, 0), 1: (0, 1), 2: (1, 0), 3: (0, -1), 4: (1, 1), 5: (-1, -1), 6: (1, -1), 7: (-1, 1)}
+
+EXERCISE_6_9_2 = {0: (-1, 0), 1: (0, 1), 2: (1, 0), 3: (0, -1), 4: (1, 1), 5: (-1, -1), 6: (1, -1), 7: (-1, 1), 8: (0, 0)}
+
+# type of action North, South, East, West movement
+MAP_ACTION = EXERCISE_6_9_1 
 
 
 class Environment:
     def __init__(self):
         self.start, self.goal = (3, 0), (3, 7)
         self.wind = [0, 0, 0, 1, 1, 1, 2, 2, 1, 0]
+        self.stochastic_wind = [0, -1, 1]
 
     def get_wind_value(self, x, y):
         return self.wind[y]
+
+    def stochastic_wind_direction(self, x, y):
+        if self.wind[y] == 0:
+            return 0
+        return self.stochastic_wind[np.random.randint(3)]
 
     def get_start(self):
         return self.start
@@ -29,14 +42,17 @@ class Environment:
     def is_goal(self, x, y):
         return x == self.goal[0] and y == self.goal[1]
 
-    def get_new_pos(self, x, y, a):
+    def get_new_pos(self, x, y, a, stochastic=False):
         xmove, ymove = MAP_ACTION[a]
         xnew, ynew = x+xmove, y+ymove
         rwd = -1
         if xnew < 0 or ynew < 0 or xnew >= WIDTH or ynew >= LENGTH:
             return x, y, rwd
         wind_value = self.get_wind_value(x, y)
-        return max(0, xnew-wind_value), ynew, rwd
+        if stochastic:
+            wind_value += self.stochastic_wind_direction(x, y)
+        xnew = min(WIDTH-1, max(0, xnew-wind_value))
+        return xnew, ynew, rwd
 
 
 class QState:
@@ -90,13 +106,13 @@ class TD0:
                 q_greedy = qa
         self.policy[x][y] = q_greedy.a
 
-    def estimate(self, episodes=3000):
+    def estimate(self, episodes=3000, stochastic=False):
         for ep in range(1, episodes+1):
             start_x, start_y = self.env.get_start()
             curr_action = self.select_action(start_x, start_y)
             q = self.map_id_to_qstate[QState(start_x, start_y, curr_action).hash()]
             while not self.env.is_goal(q.x, q.y):
-                next_x, next_y, rwd = self.env.get_new_pos(q.x, q.y, q.a)
+                next_x, next_y, rwd = self.env.get_new_pos(q.x, q.y, q.a, stochastic)
                 next_a = self.select_action(next_x, next_y)
                 next_q = self.map_id_to_qstate[QState(next_x, next_y, next_a).hash()]
                 q.update(rwd, next_q.q)
@@ -104,13 +120,19 @@ class TD0:
                 q = next_q
         return self.policy
 
+
 def save_figure(policy, env):
     X, Y = [], []
     x, y = env.get_start()
     X.append(y+0.5); Y.append(6-x+0.5)
+    dist = 1
     while not env.is_goal(x, y):
         x, y, _ = env.get_new_pos(x, y, policy[x][y])
         X.append(y+0.5); Y.append(6-x+0.5)
+        dist += 1
+        if dist > MAX_DISTANCE:
+            print("Not able to find path from start to goal. More episodes are needed to converge")
+            break
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ax.plot(X, Y)
@@ -119,12 +141,13 @@ def save_figure(policy, env):
     ax.set_xticks(xtick)
     ax.set_yticks(ytick)
     plt.grid(which='major')
-    plt.savefig("figure6_5.png")
+    plt.savefig("figure6_9_1.png")
+
 
 def main():
     env = Environment()
     td0 = TD0(env)
-    policy = td0.estimate(10000)
+    policy = td0.estimate(8000, stochastic=False)
     save_figure(policy, env)
 
 
